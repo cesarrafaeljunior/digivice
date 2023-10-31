0fa001b50119403110a90358144fc60602581b56
César Rafael <cesarrdev@gmail.com>
2023-10-31 10:01:22 -0300

    feat: Created digimon-list component

---
+++ src/app/digimon-list/digimon-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-digimon-list',
+  templateUrl: './digimon-list.component.html',
+  styleUrls: ['./digimon-list.component.css']
+})
+export class DigimonListComponent {
+
+}
