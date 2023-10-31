966830e5977884d0effdb70e8b345f0948ccf6e8
César Rafael <cesarrdev@gmail.com>
2023-10-31 10:01:05 -0300

    feat: Created digimon-car component

---
+++ src/app/digimon-card/digimon-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-digimon-card',
+  templateUrl: './digimon-card.component.html',
+  styleUrls: ['./digimon-card.component.css']
+})
+export class DigimonCardComponent {
+
+}
