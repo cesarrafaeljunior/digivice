634755d982061a03cae0a358366f7cfdbe3d16c3
César Rafael <cesarrdev@gmail.com>
2023-10-31 15:25:08 -0300

    Feat: Added function getDigimonByName

--- src/app/services/digimon.service.ts
+++ src/app/services/digimon.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { iDigimon } from '../digimon-list/digimon-list.component';
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +13,9 @@ export class DigimonService {
 
   getAllDigimons(): Observable<any> {
     return this.httpClient.get(this.apiUrl);
+  }
+
+  getDigimonByName(name: string): Observable<any> {
+    return this.httpClient.get(`${this.apiUrl}/name/${name}`);
   }
 }
