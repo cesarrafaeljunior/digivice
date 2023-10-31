2e2f898511f4de48439682cb1696d03e12a5ca9a
César Rafael <cesarrdev@gmail.com>
2023-10-31 10:06:34 -0300

    feat: Add digimon-car, digimon-list and digimon-device components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DigimonListComponent } from './digimon-list/digimon-list.component';
+import { DigimonCardComponent } from './digimon-card/digimon-card.component';
+import { DigiviceDeviceComponent } from './digivice-device/digivice-device.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DigimonListComponent,
+    DigimonCardComponent,
+    DigiviceDeviceComponent
   ],
   imports: [
     BrowserModule
