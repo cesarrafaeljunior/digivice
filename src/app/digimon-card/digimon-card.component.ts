9dddebf2e766fbaf341d8f63feccc22fcfe10054
César Rafael <cesarrdev@gmail.com>
2023-10-31 15:21:26 -0300

    feat: Created SelectedDigimonService

--- src/app/digimon-card/digimon-card.component.ts
+++ src/app/digimon-card/digimon-card.component.ts
@@ -1,10 +1,19 @@
 import { Component, Input } from '@angular/core';
+import { DigimonService } from '../services/digimon.service';
+import { SelectedDigimonService } from '../services/selected-digimon.service';
+import { iDigimon } from '../digimon-list/digimon-list.component';
 
 @Component({
   selector: 'app-digimon-card',
   templateUrl: './digimon-card.component.html',
-  styleUrls: ['./digimon-card.component.css']
+  styleUrls: ['./digimon-card.component.css'],
 })
 export class DigimonCardComponent {
-  @Input() digimonData: any
+  @Input() digimonData: any;
+
+  constructor(private selectedDigimonService: SelectedDigimonService) {}
+
+  public onCardClick(digimon: iDigimon) {
+    this.selectedDigimonService.setSelectedDigimon(digimon);
+  }
 }
