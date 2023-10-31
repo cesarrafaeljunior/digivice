4a653a39380895b1d2e5a7ef1505377d11fd2ff0
César Rafael <cesarrdev@gmail.com>
2023-10-31 15:24:50 -0300

    Feat: Created a function for selectedDigimon

--- src/app/digivice-device/digivice-device.component.ts
+++ src/app/digivice-device/digivice-device.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { SelectedDigimonService } from '../services/selected-digimon.service';
+import { iDigimon } from '../digimon-list/digimon-list.component';
+import { DigimonService } from '../services/digimon.service';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-digivice-device',
   templateUrl: './digivice-device.component.html',
-  styleUrls: ['./digivice-device.component.css']
+  styleUrls: ['./digivice-device.component.css'],
 })
 export class DigiviceDeviceComponent {
+  selectedDigimon: iDigimon | null = null;
 
+  constructor(
+    private digimonService: DigimonService,
+    private selectedDigimonService: SelectedDigimonService
+  ) {}
+
+  ngOnInit() {
+    this.selectedDigimonService.selectedDigimon$.subscribe((digimonList$) => {
+      this.selectedDigimon = digimonList$;
+    });
+  }
 }
