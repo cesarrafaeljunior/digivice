192dcd10f02f78cc6ec859e0bc4400b8553ee0e3
César Rafael <cesarrdev@gmail.com>
2023-10-31 10:01:45 -0300

    feat: Createdt digivice-device component

---
+++ src/app/digivice-device/digivice-device.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-digivice-device',
+  templateUrl: './digivice-device.component.html',
+  styleUrls: ['./digivice-device.component.css']
+})
+export class DigiviceDeviceComponent {
+
+}
